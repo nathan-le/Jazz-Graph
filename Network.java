56f86c9fc4e5534490b0c82139a49acecacc146a
Nathan Le <nathan@Nathans-MacBook-Air.local>
2018-05-22 12:40:18 -0700

    updating java code

--- Network.java
+++ Network.java
@@ -3,27 +3,28 @@ import java.util.HashMap;
 public class Network {
 	
 	private String name;
-	private HashMap<String, Node> nodes = new HashMap<String, Node>();
+	private HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
 	
 	public Network(String name) {
 		this.name = name;
 	}
 
+
 	public String getName() {
 		return this.name;
 	}
 
-	public void addNode(Node entry) {
-		this.nodes.put(entry.getName(), entry);
-	}
-
-	public Node getNode(String entry) {
-		if (!this.nodes.containsKey(entry)) {
+	public Node getNode(int id) {
+		if (!this.nodes.containsKey(id)) {
 			System.out.println("Invalid Entry");
 			return null;
 		} else {
-			return this.nodes.get(entry);	
+			return this.nodes.get(id);	
 		}
+	}
+
+	public void addNode(Node entry) {
+		this.nodes.put(entry.getId(), entry);
 	}
 
 
--- Node.java
+++ Node.java
@@ -3,25 +3,19 @@ import java.util.*;
 
 public class Node {
 	
-	private String name;
+	private int id;
 	private Album album;
-	private HashMap<String, Album> out = new HashMap<String, Album>();
-	private HashMap<String, Album> in = new HashMap<String, Album>();
+	private HashMap<Integer, Node> out;
 	private int group;
 	
-	public Node(String name, Album album, Album[] outEdges, Album[] inEdges, int group) {
-		this.name = name;
+	public Node(int id, Album album, HashMap<Integer, Node> outEdges, int group) {
+		this.id = id;
 		this.album = album;
-		for (int i = 0; i < outEdges.length; i += 1) {
-			out.put(outEdges[i].getName(), outEdges[i]);
-		}
-		for (int i = 0; i < inEdges.length; i += 1) {
-			in.put(inEdges[i].getName(), inEdges[i]);
-		}
+		this.out = outEdges;
 		this.group = group;
 	}
 
-	public String getName() {
+	public int getId() {
 		return this.name;
 	}
 
@@ -29,41 +23,28 @@ public class Node {
 		return this.album;
 	}
 	public Set getOut() {
-		return out.entrySet();
-	}
-	public Set getIn() {
-		return in.entrySet();
-	}
-
-	public Album where(String name) {
-		if (out.containsKey(name)) {
-			System.out.printf("Found %s in Out\n", name);
-			return out.get(name);
-		} else if (in.containsKey(name)) {
-			System.out.printf("Found %s in In\n", name);
-			return in.get(name);
-		} else {
-			System.out.printf("Entry not found\n");
-			return null;
-		}
+		return this.out.entrySet();
 	}
 
 	public int getGroup() {
 		return this.group;
 	}
 
+	public Node getConnectedNode(int id) {
+		if (!this.out.containsKey(id)) {
+			System.out.println("Invalid Entry");
+			return null;
+		} else {
+			return this.nodes.get(id);	
+		}
+	}
+
 	public void addOut(Album entry) {
 		out.put(entry.getName(), entry);
-	}
-	public void addIn(Album entry) {
-		in.put(entry.getName(), entry);
 	}
 
 	public Album removeOut(Album entry) {
 		return out.remove(entry.getName());
-	}
-	public Album removeIn(Album entry) {
-		return in.remove(entry.getName());
 	}
 
 }
