5546dc3c03374e8622471c95c1d3be4384b2949a
Nathan Le <nathan@Nathans-MacBook-Air.local>
2018-05-28 11:17:19 -0700

    added cannonball

--- Main.java
+++ Main.java
@@ -4,15 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Album undercurrent = new Album("Undercurrent", 1967, new String[]{"Bill Evans", "Jim Hall"}, new String[]{"Bill Evans", "Jim Hall"});
-		System.out.println(undercurrent.getName());
-		System.out.println(undercurrent.getYear());
-		System.out.println(undercurrent.getArtists());
-		
 
-		for (int i = 0; i < undercurrent.getLeader().length; i += 1 ) {
-			System.out.println(undercurrent.getLeader()[i]);
-		}
 
 	}
 
